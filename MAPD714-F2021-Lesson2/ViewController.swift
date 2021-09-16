f45cf7b623e9042251754903ba83cb2f436a1c26
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2021-09-16 19:55:04 -0400

    Initial Commit

---
+++ MAPD714-F2021-Lesson2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MAPD714-F2021-Lesson2
+//
+//  Created by Tom Tsiliopoulos on 2021-09-16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
