6f92a510079cb11927ee449b356448ed9d63d0d5
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2021-09-16 21:02:42 -0400

    add OneButton

---
+++ MAPD714-F2021-Lesson2/MainViewController.swift
@@ -0,0 +1,29 @@
+/*
+ Author    : Tom Tsiliopoulos
+ Student ID: 300849000
+ Date      : Sept 16, 2021
+ */
+
+import UIKit
+
+class MainViewController: UIViewController
+{
+    // variable declarations
+    @IBOutlet weak var HelloLabel: UILabel!
+    
+    // LifeCycle Function
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+
+    // Event Handler for the OneButton press event
+    @IBAction func OnOneButton_Pressed(_ sender: UIButton)
+    {
+        // ternary operator
+        HelloLabel.text = (HelloLabel.text == "Hello!") ? "Goodbye!" : "Hello!"
+        print(HelloLabel.text!)
+    }
+    
+}
+
